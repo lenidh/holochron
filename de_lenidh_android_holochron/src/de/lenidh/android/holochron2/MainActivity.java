89d717de653274da4082ce08f45be616bf1f841a
Moritz Heindl <lenidh@gmail.com>
2013-04-27 00:22:46 +0200

    Begin work on holochron 2 holochron 2 is a complete rewrite of holochron.

---
+++ de_lenidh_android_holochron/src/de/lenidh/android/holochron2/MainActivity.java
@@ -0,0 +1,25 @@
+package de.lenidh.android.holochron2;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		//setContentView(new DigitalDisplay(this));
+	}
+
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.main, menu);
+//		return true;
+//	}
+
+}
