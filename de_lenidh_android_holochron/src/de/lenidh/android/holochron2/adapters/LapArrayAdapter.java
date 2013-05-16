08296acda53570095affb2db020e9b468328fb01
Moritz Heindl <lenidh@gmail.com>
2013-05-16 23:09:12 +0200

    Improve lap representation.

--- de_lenidh_android_holochron/src/de/lenidh/android/holochron2/adapters/LapArrayAdapter.java
+++ de_lenidh_android_holochron/src/de/lenidh/android/holochron2/adapters/LapArrayAdapter.java
@@ -1,8 +1,8 @@
 package de.lenidh.android.holochron2.adapters;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import android.util.Log;
 import de.lenidh.android.holochron2.R;
 import de.lenidh.libzeitmesser.stopwatch.Lap;
 import android.content.Context;
@@ -13,22 +13,34 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 public class LapArrayAdapter extends ArrayAdapter<Lap> {
+
+	private static final String TAG = "LapArrayAdapter";
 	
 	private class ViewHolder {
-		public TextView elapsedTimeView;
-		public TextView elapsedTimeDiffView;
-		public TextView lapTimeView;
-		public TextView lapTimeDiffView;
+		public TextView numberView;
+		public TextView timeView;
+		public TextView diffView;
+	}
+
+	public enum Mode {
+		lapTime,
+		elapsedTime,
 	}
 
 	private LayoutInflater inflater;
 	private List<Lap> values;
-	
+	private Mode mode;
+
 	public LapArrayAdapter(Context context, List<Lap> values) {
+		this(context, values, Mode.elapsedTime);
+	}
+
+	public LapArrayAdapter(Context context, List<Lap> values, Mode mode) {
 		super(context, R.layout.lap_listitem, values);
 		
 		this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		this.values = values;
+		this.mode = mode;
 	}
 
 	@Override
@@ -42,14 +54,66 @@ public class LapArrayAdapter extends ArrayAdapter<Lap> {
 			convertView = this.inflater.inflate(R.layout.lap_listitem, parent, false);
 			
 			holder = new ViewHolder();
-			holder.elapsedTimeView = (TextView)convertView.findViewById(R.id.lap_text);
+			holder.numberView = (TextView)convertView.findViewById(R.id.txt_number);
+			holder.timeView = (TextView)convertView.findViewById(R.id.txt_time);
+			holder.diffView = (TextView)convertView.findViewById(R.id.txt_diff);
 			
 			convertView.setTag(holder);
 		}
-		
-		holder.elapsedTimeView.setText(String.valueOf(this.values.get(position).getElapsedTime()));
+
+		holder.numberView.setText(Integer.toString(position));
+
+		switch (this.mode) {
+			case elapsedTime:
+				holder.timeView.setText(this.formatTime(this.values.get(position).getElapsedTime(), false));
+				holder.diffView.setText(this.formatTime(this.values.get(position).getElapsedTimeDiff(), true));
+				break;
+			case lapTime:
+				holder.timeView.setText(this.formatTime(this.values.get(position).getLapTime(), false));
+				holder.diffView.setText(this.formatTime(this.values.get(position).getLapTimeDiff(), true));
+				break;
+		}
 		
 		return convertView;
+	}
+
+	/**
+	 * This method creates a formatted time string from a millisecond value.
+	 * Format: [[[[[h]h:]m]m:]s]s.ms (e.g. 00:00:00.000)
+	 * @param time in milliseconds
+	 * @param trim If true, leading zeros and separators are trimmed. (e.g. 00:00:00.000 becomes 0.000)
+	 * @return formatted time string
+	 */
+	private String formatTime(long time, boolean trim) {
+		StringBuilder timeFormat = new StringBuilder();
+		short digits[] = {
+				(short) (time / 36000000 % 6),    // hours: tens
+				(short) (time / 3600000 % 10),    //        ones
+				-1,                               // separator: ':'
+				(short) (time / 600000 % 6),      // minutes: tens
+				(short) (time / 60000 % 10),      //          ones
+				-1,                               // separator: ':'
+				(short) (time / 10000 % 6),       // seconds: tens
+				(short) (time / 1000 % 10),       //          ones
+				-2,                               // separator: '.'
+				(short) (time / 100 % 10),        // milliseconds: hundreds
+				(short) (time / 10 % 10),         //               tens
+				(short) (time % 10),              //               ones
+		};
+		int index = 0;
+
+		// Skip leading zeros and separators.
+		while (trim && index < 7 && digits[index] <= 0) index++;
+
+		while (index < 12) {
+			if(digits[index] >= 0) timeFormat.append(digits[index]);
+			else if(digits[index] == -1) timeFormat.append(':');
+			else if(digits[index] == -2) timeFormat.append('.');
+			else Log.e(TAG, String.format("Invalid value while formatting: time: %d, index: %d, value: %d", time, index, digits[index]));
+			index++;
+		}
+
+		return timeFormat.toString();
 	}
 
 }
--- de_lenidh_android_holochron/src/de/lenidh/android/holochron2/ui/MainActivity.java
+++ de_lenidh_android_holochron/src/de/lenidh/android/holochron2/ui/MainActivity.java
@@ -1,33 +1,19 @@
 package de.lenidh.android.holochron2.ui;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.actionbarsherlock.app.SherlockActivity;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.Button;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.app.SherlockListFragment;
-
 import de.lenidh.android.holochron2.R;
-import de.lenidh.android.holochron2.R.id;
-import de.lenidh.android.holochron2.R.layout;
 import de.lenidh.android.holochron2.adapters.LapArrayAdapter;
 import de.lenidh.android.holochron2.adapters.LapPagerAdapter;
 import de.lenidh.android.holochron2.controls.DigitalDisplay;
-import de.lenidh.libzeitmesser.stopwatch.Display;
-import de.lenidh.libzeitmesser.stopwatch.Lap;
-import de.lenidh.libzeitmesser.stopwatch.LapContainer;
-import de.lenidh.libzeitmesser.stopwatch.SystemTime;
-import de.lenidh.libzeitmesser.stopwatch.Watch;
+import de.lenidh.libzeitmesser.stopwatch.*;
 
-import android.os.Bundle;
-import android.os.SystemClock;
-import android.app.Activity;
-import android.app.ListFragment;
-import android.support.v4.view.ViewPager;
-import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ListView;
+import java.util.List;
 
 public class MainActivity extends SherlockFragmentActivity implements Display {
 	
@@ -72,10 +58,10 @@ public class MainActivity extends SherlockFragmentActivity implements Display {
 		this.watch.addDisplay(this);
 
 		this.lapContainer = this.watch.getLapContainer();
-		this.lapTimeItems = this.lapContainer.toList();
+		this.lapTimeItems = this.lapContainer.toList(); // TODO: List in lap time order. Needs modification of libZeitmesser.
 		this.elapsedTimeItems = this.lapContainer.toList();
-		this.lapTimeArrayAdapter = new LapArrayAdapter(this, this.lapTimeItems);
-		this.elapsedTimeArrayAdapter = new LapArrayAdapter(this, this.elapsedTimeItems);
+		this.lapTimeArrayAdapter = new LapArrayAdapter(this, this.lapTimeItems, LapArrayAdapter.Mode.lapTime);
+		this.elapsedTimeArrayAdapter = new LapArrayAdapter(this, this.elapsedTimeItems, LapArrayAdapter.Mode.elapsedTime);
 		((SherlockListFragment)this.lapPagerAdapter.getItem(0)).setListAdapter(this.lapTimeArrayAdapter);
 		((SherlockListFragment)this.lapPagerAdapter.getItem(1)).setListAdapter(this.elapsedTimeArrayAdapter);
 		
@@ -94,7 +80,7 @@ public class MainActivity extends SherlockFragmentActivity implements Display {
 		this.btnExtra.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
-			public void onClick(View arg0) {
+			public void onClick(View v) {
 				if(watch.isRunning()) {
 					watch.record();
 				} else {
